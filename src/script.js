737f62575a0eb6934925c29a23f6c54e94a37489
Davi Pereira <132598131+Daavipf@users.noreply.github.com>
2023-10-03 11:51:38 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 const toggleDarkBtn = document.querySelector('#toggleDarkBtn')
 const toggleLightBtn = document.querySelector('#toggleLightBtn')
+const hamburguerMenuBtn = document.querySelector('#hamburguerMenuBtn')
+const hamburguerMenuReturnBtn = document.querySelector('#hamburguerMenuReturnBtn')
 
 toggleDarkBtn.addEventListener('click', darkMode)
 toggleLightBtn.addEventListener('click', lightMode)
+hamburguerMenuBtn.addEventListener('click', openHmbgMenu)
+hamburguerMenuReturnBtn.addEventListener('click', closeHmbgMenu)
 
 function darkMode(){
     document.querySelector('html').classList.add('dark')
@@ -14,4 +18,12 @@ function lightMode(){
     document.querySelector('html').classList.remove('dark')
     toggleDarkBtn.classList.remove('hidden')
     toggleLightBtn.classList.add('hidden')
+}
+
+function openHmbgMenu(){
+    document.querySelector('#hamburguerMenu').classList.remove('hidden')
+}
+
+function closeHmbgMenu(){
+    document.querySelector("#hamburguerMenu").classList.add('hidden')
 }
